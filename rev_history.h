d54204763ef56a53bde4b171aaecd3cf9abfede8
Noggin01 <henry.g.mcdonald@gmail.com>
2014-06-17 00:31:38 +0000

    added revision history file

---
+++ rev_history.h
@@ -0,0 +1,15 @@
+/**********************************************************************
+File:  rev_history.h
+
+**********************************************************************/
+
+#define FIRMWARE_MAJOR		0
+#define FIRMWARE_MINOR		1
+#define FIMRWARE_REVISION	0
+
+/* Description of changes. ********************************************
+
+*** 16JUN14 *** Ver 0.1.0 *** HGM
+1. Initial release.  Not very user friendly.
+
+*/
